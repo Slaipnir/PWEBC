9bc46e554f30145aa0a60f4f0ec69074ce255fd3
PAUL VILLEMAIN <villemai@iut.univ-paris5.fr>
2018-02-21 15:05:59 +0100

    draggable/droppable

--- mapJS.js
+++ mapJS.js
@@ -1,6 +1,59 @@
 window.onload = function () {
 	var map = L.map('map').setView([48.858376, 2.294442],12);
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+	
+	//Rendre draggable les div des pays
+	$( "#France" ).draggable({ revert: "valid" });
+	$( "#Canada" ).draggable({ revert: "valid" });
+	$( "#Italie" ).draggable({ revert: "valid" });
+	$( "#Belgique" ).draggable({ revert: "valid" });
+	$( "#Japan" ).draggable({ revert: "valid" });
+	
+	//Rendre la map droppable
+	 $( "#map" ).droppable({
+		 
+		 //Evenement lors du drop
+		drop: function( event, ui ) {
+			
+			//Recupère l'id du block div "dropped" dans la map
+			var IdPays = ui.draggable.attr("id");
+			
+			var chaine="";
+			chaine+="Pays : "+IdPays+"</br>";
+			
+			//Requete AJAX pour récupérer les coordonnées (lati, longi) du pays
+			$.ajax({
+			    type: 'GET',
+			    url: "http://nominatim.openstreetmap.org/search",
+			    dataType: 'jsonp',
+			    jsonpCallback: 'data',
+			    data: { format: "json", limit: 1,country: IdPays,json_callback: 'data' },
+			    error: function(xhr, status, error) {
+						alert("ERROR "+error);
+			    },
+			    success: function(data){
+				//récupérer les coordonnées (lati, longi) du pays dans les données json provenant du serveur
+					var lati = '';
+					var longi = '';
+					$.each(data, function() {
+						lati = this['lat'] ;
+						longi = this['lon'] ;
+				});
+				
+				//affichage des infos
+				chaine+="Latitude : "+lati+"</br>";
+				chaine+="Longitute : "+longi+"</br>";
+				$( "#info" ).html(chaine);
+				
+				//MAJ de la map à la position (lati, longi) du pays
+				map.panTo(new L.LatLng(lati, longi));		
+				
+			    }
+			});
+			
+			
+		}
+	});
 }
 
 
