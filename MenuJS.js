6c858b24ce115dfcd24327988fb883dbf6f2e4ce
MATHIEU PATRY <patry@iut.univ-paris5.fr>
2018-03-13 18:09:40 +0100

    ifhi

--- MenuJS.js
+++ MenuJS.js
@@ -1,20 +1,39 @@
 $( function() {
+		
     $( "#accordion" ).accordion({
 	heightStyle: "content"
 	});
 	$( "#field input" ).checkboxradio();
 	$( "#pbBad" ).progressbar({
-      value: 35,
+      value: 0,
+	  create: function(event,ui){
+		$("#hBad").text("Nombre de Mauvaises réponses max : " + 3 );
+	  }
     });
 	$( "#pbQuest" ).progressbar({
-      value: 35,
+      value: 0,
+	  max: 10,
+	  create: function(event,ui){
+		$("#hQuest").text("Nombre de Question a répondre : " + 10);
+	  }
     });
-	$("#map1").show();
-	$("#map2").hide();
-	$("#map3").hide();
-	$("#spinner").spinner();
 	
-	$("#spinner").spinner( "value", 5 );
+	$("#spinner").spinner({
+	min:0,
+	max:10,
+	change: function(event,ui){
+		var x = this;
+		$( "#pbQuest" ).progressbar("destroy");
+		$( "#pbQuest" ).progressbar({
+		max:x.value,
+		create: function(event,ui){
+		$("#hBad").text("Nombre de Mauvaises réponses max : " + x.value);
+		}
+		});
+	}
+	});
+	
+	$("#spinner").spinner( "value", 3 );
 	//document.getElementById("spinner").defaultValue = "10";
 	
 	
@@ -22,26 +41,24 @@ $( function() {
 	//var themeClass = $( "#spinner" ).spinner( "option", "classes.ui-spinner" );
 	//console.log(themeClass);
 	
+	var map = L.map("map1").setView([47, 2],6);
+	var couche = new L.TileLayer('http://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png')
+	map.addLayer(couche);
+	
+	
 	$("#radio-1").click(function(){
-		$("#map1").show();
-		$("#map2").hide();
-		$("#map3").hide();
+		map.setView(new L.LatLng(47, 2),6);	
 	});
 	
 	$("#radio-2").click(function(){
-		$("#map1").hide();
-		$("#map2").show();
-		$("#map3").hide();
+		map.setView(new L.LatLng(37, -100),4);
 	});
 	
 	$("#radio-3").click(function(){
-		$("#map1").hide();
-		$("#map2").hide();
-		$("#map3").show();
+		
 	});
 	
-	var map = L.map("map1").setView([47, 2],5);
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+	
 } );
 
   
